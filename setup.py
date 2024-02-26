f216791059213dad8127392c1c30c46aa8e3594c
ThomasAlscher1991 <61122685+ThomasAlscher1991@users.noreply.github.com>
2024-02-26 10:01:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=['dasscotest'],
     package_dir={'':'src'},
     package_data={"dasscotest": ["*.json"]},
-    version='1.0.8',
+    version='1.0.9',
     author='Thomas Alscher',
     author_email='',
     python_requires='>3.9.0',
